696adf6d2050bf6026f22eee2a719a5be008cdb3
smtr42 <simteiva@gmailcom>
2020-05-18 11:12:15 +0200

    Add models structure in the product apps

--- products/models.py
+++ products/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Category(models.Model):
+    """handle the categories information."""
+
+    category_name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.category_name
+
+
+class Product(models.Model):
+    """Handle the Product information."""
+
+    barcode = models.CharField(max_length=13, unique=True)
+    product_name = models.CharField(max_length=255)
+    nutriscore = models.CharField(max_length=1)
+    url = models.CharField(max_length=255)
+    image_url = models.CharField(max_length=255)
+    image_nut_url = models.CharField(max_length=255)
+    categories = models.ManyToManyField("Category", related_name="products")
+
+    def __str__(self):
+        return self.product_name
+
+
+class Favorite(models.Model):
+    """This will regroup the product and its substitute associated
+    with a user.
+    """
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='favorites', on_delete=models.CASCADE, )
+    product = models.ForeignKey("Product", related_name='favorites_as_product', on_delete=models.CASCADE, )
+    substitute = models.ForeignKey("Product", related_name='fav_substitute', on_delete=models.CASCADE, )
