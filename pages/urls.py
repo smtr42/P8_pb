c726f0e0864c51dfeb1e84b200e8fe049c5a3630
smtr42 <simteiva@gmailcom>
2020-05-23 17:24:08 +0200

    Add minor modifications

--- pages/urls.py
+++ pages/urls.py
@@ -1,12 +1,7 @@
 from django.urls import path
 
-# from . import views
-#
-# urlpatterns = [
-#     path('', views.HomePageView.as_view(), name='index'),
-# ]
-
 from . import views
+
 app_name = 'pages'
 
 urlpatterns = [
