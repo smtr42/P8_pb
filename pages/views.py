54f545abb7e488cf8607fa136c5a29d60caa4fc1
smtr42 <simteiva@gmailcom>
2020-05-25 10:50:43 +0200

    Fix signup not showing

--- pages/views.py
+++ pages/views.py
@@ -2,4 +2,5 @@ from django.views.generic import TemplateView
 
 
 class HomePageView(TemplateView):
+    # https://docs.djangoproject.com/fr/2.2/topics/class-based-views/#subclassing-generic-views
     template_name = 'pages/index.html'
--- users/views.py
+++ users/views.py
@@ -3,7 +3,8 @@ from django.views.generic.edit import CreateView
 
 from .forms import UserCreationForm
 
+
 class SignUp(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'signup.html'
+    template_name = 'pages/signup.html'
