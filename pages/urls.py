6e1820f19e9372640639311222c4d9b3c359c18e
smtr42 <simteiva@gmailcom>
2020-06-07 17:38:02 +0200

    Add favicon and story

--- pages/urls.py
+++ pages/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import HomePageView
+from .views import HomePageView, NoticePageView
 
 app_name = 'pages'
 urlpatterns = [
     path('', HomePageView.as_view(), name='index'),
+    path('notice', NoticePageView.as_view(), name='notice'),
 ]
