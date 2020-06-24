72f0612ff3c9ea2b44b26cab80fc67f304e1685f
Teiva <t.simonnet@mf2i.fr>
2020-06-24 20:56:33 +0200

    Deleted useless comment

--- products/views.py
+++ products/views.py
@@ -5,24 +5,6 @@ from django.shortcuts import render
 from products.forms import SearchForm
 from products.managers import ProductManager
 
-# def search_product(request):
-#     # if this is a POST request we need to process the form data
-#     if request.method == 'GET':
-#         # create a form instance and populate it with data from the request:
-#         form = SearchForm(request.GET)
-#         # check whether it's valid:
-#         if form.is_valid():
-#             data = ProductManager.search_from_user_input(
-#                 form.cleaned_data)
-#             return render(request, 'products/sub_list.html', data)
-#             # return HttpResponseRedirect('products/sub_list.html')
-#         else:
-#             raise Http404
-#     # if a GET (or any other method) we'll create a blank form
-#     else:
-#         form = SearchForm()
-#     return render(request, 'products/sub_list.html', {'form': form})
-
 
 def sub_list(request):
     if request.method == "POST":
