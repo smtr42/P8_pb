b02b01e17610e2068bb412135596b6277f98530f
Teiva <t.simonnet@mf2i.fr>
2020-06-25 17:31:43 +0200

    WIP fix static project_dir

--- core/settings.py
+++ core/settings.py
@@ -153,10 +153,12 @@ ACCOUNT_EMAIL_VERIFICATION = "none"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, "staticfiles"))
+STATIC_ROOT = os.path.normpath(os.path.join(PROJECT_DIR, "staticfiles"))
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(PROJECT_DIR, "static"),
 ]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
