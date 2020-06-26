3a000c3609aa6c6f2d2edbb979b943a67c9c4898
Teiva <t.simonnet@mf2i.fr>
2020-06-26 10:54:43 +0200

    Debug

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,9 @@ AUTH_USER_MODEL = "users.User"
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False if os.environ.get("ENV", "development") == "production" else True
+# DEBUG = False if os.environ.get("ENV", "development") == "production" else True
+
+DEBUG = True
 
 ALLOWED_HOSTS = [".herokuapps.com", "localhost", "127.0.0.1"]
 
