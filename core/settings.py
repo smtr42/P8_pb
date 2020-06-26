07f2faf5391346366dc2d966f7834d46113d72f7
Teiva <t.simonnet@mf2i.fr>
2020-06-26 10:44:27 +0200

    Fix decouple not recognize by heroku

--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
-from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -23,11 +22,7 @@ AUTH_USER_MODEL = "users.User"
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    config("SECRET_KEY")
-    if config("SECRET_KEY") != ""
-    else os.environ.get("SECRET_KEY")
-)
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False if os.environ.get("ENV", "development") == "production" else True
