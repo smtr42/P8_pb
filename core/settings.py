fc11aa75e60c549c67399603f50dc287d5dcf5af
Teiva <t.simonnet@mf2i.fr>
2020-06-26 11:35:27 +0200

    add debug

--- core/settings.py
+++ core/settings.py
@@ -111,6 +111,23 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
